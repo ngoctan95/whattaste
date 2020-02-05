f66282211aa2d154f4c2df19b1a472cae08d5fdb
Nashel Ura <nguyenngoctan44@gmail.com>
2020-02-05 10:34:48 +0700

    Add app icon for Android and iOS

--- whattaste/lib/main.dart
+++ whattaste/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Hello, what''s taste today?'),
+      home: MyHomePage(title: 'Hello, what\'s taste today?'),
     );
   }
 }
